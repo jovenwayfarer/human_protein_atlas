ee1273fd6ccd878e347e1c2c772cfa0e990b8d52
jovenwayfarer <47921506+jovenwayfarer@users.noreply.github.com>
2021-05-12 03:26:54 +0600

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -10,6 +10,6 @@ external_data_imgs ="$5"
 hpa_mask_dir = $"6"
 
 
-python get_cell_comp_data.py --train_csv "$competition_data_csv" --train_data "$competition_data_imgs" --hpa_masks "$hpa_mask_dir" --cell_tiles $"hpa_mask_dir"
-python get_cell_Riedel_data.py --train_csv "$external_data_csv" --train_data "$external_data_imgs"  --cell_tiles $"hpa_mask_dir"
-create_folds.py
+python data_preprocessing/get_cell_comp_data.py --train_csv "$competition_data_csv" --train_data "$competition_data_imgs" --hpa_masks "$hpa_mask_dir" --cell_tiles $"hpa_mask_dir"
+python data_preprocessing/get_cell_Riedel_data.py --train_csv "$external_data_csv" --train_data "$external_data_imgs"  --cell_tiles $"hpa_mask_dir"
+python data_preprocessing/create_folds.py
